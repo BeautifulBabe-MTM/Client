63ac27a21a91c8028c307b8d6988f43842d8a53f
Daniel Yushko <dan.dyushko.hgjh95@gmail.com>
2021-09-08 11:15:36 +0300

    server

--- Server/Client/Program.cs
+++ Server/Client/Program.cs
@@ -4,8 +4,13 @@ namespace Client
 {
     class Program
     {
+        static string ipAddress = "127.0.0.1";
+        static int port = 8000;
         static void Main(string[] args)
         {
+
+
+
             Console.WriteLine("Hello World!");
         }
     }
